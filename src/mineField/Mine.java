d387c6b6ebbd7a9ad0496c55be1a7da5384ef73b
Alex <alexanderchang140@gmail.com>
2025-03-10 19:53:10 -0700

    feat: add basic mine model

--- src/mineField/Mine.java
+++ src/mineField/Mine.java
@@ -2,8 +2,98 @@ package mineField;
 
 import mvc.Model;
 
-public class Mine extends Model {
-    public Mine() {
+import java.util.Random;
 
+public class Mine extends Model {
+    private static final int GRID_SIZE = 10;
+    private static final int PERCENT_MINED = 5;
+
+    private final boolean[][] minefield;
+    private final int[][] grid;
+    private GameState gameState = GameState.RUNNING;
+    private int x = 0;
+    private int y = 0;
+
+    public enum GameState {
+        RUNNING,
+        FAIL,
+        SUCCESS
+    }
+
+    public Mine() {
+        minefield = generateMinefield();
+        grid = new int[GRID_SIZE][GRID_SIZE];
+    }
+
+    // Assumes unwinnable grid is possible
+    // Top left and bottom right are never mined
+    private boolean[][] generateMinefield() {
+        boolean[][] minefield = new boolean[GRID_SIZE][GRID_SIZE];
+        Random rand = new Random();
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                if (row == 0 && col == 0 || successCondition(row, col)) {
+                    continue;
+                }
+                minefield[row][col] = rand.nextInt(100) < PERCENT_MINED;
+            }
+        }
+        return minefield;
+    }
+
+    public void setPosition(int x, int y) {
+        if (!isValidCell(x, y)) {
+            return;
+        }
+        gameState = isMinedCell(x, y) ? GameState.FAIL : GameState.RUNNING;
+        gameState = successCondition(x, y) ? GameState.SUCCESS : GameState.RUNNING;
+        updateCell(x, y);
+        this.x = x;
+        this.y = y;
+    }
+
+    private void updateCell(int x, int y) {
+        int val = 0;
+        val += isMinedCell(x - 1, y + 1) ? 1 : 0;
+        val += isMinedCell(x - 1, y) ? 1 : 0;
+        val += isMinedCell(x - 1, y - 1) ? 1 : 0;
+        val += isMinedCell(x, y + 1) ? 1 : 0;
+        val += isMinedCell(x, y - 1) ? 1 : 0;
+        val += isMinedCell(x + 1, y + 1) ? 1 : 0;
+        val += isMinedCell(x + 1, y) ? 1 : 0;
+        val += isMinedCell(x + 1, y - 1) ? 1 : 0;
+        grid[x][y] = val;
+    }
+
+    private boolean isMinedCell(int x, int y) {
+        if (!isValidCell(x, y)) {
+            return false;
+        }
+
+        return minefield[x][y];
+    }
+
+    private boolean successCondition(int x, int y) {
+        return x == GRID_SIZE - 1 && y == GRID_SIZE - 1;
+    }
+
+    private boolean isValidCell(int x, int y) {
+        return x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getGrid() {
+         return grid;
+    }
+
+    public GameState getGameState() {
+        return gameState;
     }
 }
