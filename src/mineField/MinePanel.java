544167465d51160d0fcf45c1edeb5fae3076ca44
Alex <alexanderchang140@gmail.com>
2025-03-15 00:05:51 -0700

    fix: center direction buttons

--- src/mineField/MinePanel.java
+++ src/mineField/MinePanel.java
@@ -26,26 +26,28 @@ public class MinePanel extends AppPanel {
         JPanel p = new JPanel();
         p.setLayout(new GridLayout(3, 3));
 
-        JButton north = new JButton("N");
         JButton northWest = new JButton("NW");
+        JButton north = new JButton("N");
         JButton northEast = new JButton("NE");
         JButton west = new JButton("W");
+        JLabel empty = new JLabel();
         JButton east = new JButton("E");
         JButton southWest = new JButton("SW");
         JButton south = new JButton("S");
         JButton southEast = new JButton("SE");
 
-        p.add(north);
         p.add(northWest);
+        p.add(north);
         p.add(northEast);
         p.add(west);
+        p.add(empty);
         p.add(east);
         p.add(southWest);
         p.add(south);
         p.add(southEast);
 
-        north.addActionListener(listener);
         northWest.addActionListener(listener);
+        north.addActionListener(listener);
         northEast.addActionListener(listener);
         west.addActionListener(listener);
         east.addActionListener(listener);
